3a603181316d14154ac47f93bef1e9c33c926bef
GideonMissoy <giddykips2030@gmail.com>
2024-06-20 00:36:12 +0300

    Hot Fix.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """determines if the boxes can be opened"""
 
+
 def canUnlockAll(boxes):
 
     if (type(boxes) is not list):
@@ -12,7 +13,7 @@ def canUnlockAll(boxes):
     keys = [0]
     for y in keys:
         for k in boxes[y]:
-            if k not in keys and k != y and k< len(boxes) and k != 0:
+            if k not in keys and k != y and k < len(boxes) and k != 0:
                 keys.append(k)
     if len(keys) == len(boxes):
         return True
